68ea405465fbd8f71d0390536162d0a98d45d5bc
avadhut <avadhutmore1998r@gmail.com>
2021-12-04 14:05:13 +0530

    added string question : 51

---
+++ questions/51_check_whether_one_string_is_rotation_of_another.py
@@ -0,0 +1,19 @@
+"""
+Question - Given a string s1 and a string s2, write a snippet to say whether s2 is a rotation of s1
+
+link - https://www.geeksforgeeks.org/a-program-to-check-if-strings-are-rotations-of-each-other/
+
+example -
+         s1 = ABCD and s2 = CDAB
+
+        answer - True
+"""
+
+def isSame(string1, string2):
+    string1 = string1 + string1
+    return True if (string1.count(string2)) else False
+
+if __name__ == "__main__":
+    string1 = 'abcd'
+    string2 = 'cdba'
+    print(isSame(string1, string2))
